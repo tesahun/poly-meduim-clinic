b68aef567530385465eb1886c1197930c43ac965
tesahun <147715411+tesahun@users.noreply.github.com>
2023-11-03 04:46:07 -0700

    Update Our.jsx

--- components/Our.jsx
+++ components/Our.jsx
@@ -10,15 +10,22 @@ const Our = () => {
       <div className="bg-gradient-to-t from-slate-100 w-full h-full">
         <h1 className="text-center uppercase text-3xl font-semibold pt-8">ይጎብኙን</h1>
         <div className="flex justify-center items-center gap-[50px] pt-20">
-          <FaFacebook size={35} style={{ color: "#0000FF" }} className="icon" />
+           <a href="https://www.facebook.com/groups/2604465439717035/?ref=share">
+            <FaFacebook
+              size={35}
+              style={{ color: "#0000FF" }}
+              className="icon"
+            />
+          </a>
 
           <BsWhatsapp size={35} style={{ color: "#008000" }} className="icon" />
-
-          <FaTelegramPlane
-            size={35}
-            style={{ color: "#4682B4" }}
-            className="icon"
-          />
+          <a href="https://t.me/+sVhBVs3u7Wg3NjFk">
+            <FaTelegramPlane
+              size={35}
+              style={{ color: "#4682B4" }}
+              className="icon"
+            />
+          </a>
         </div>
       </div>
     </div>
